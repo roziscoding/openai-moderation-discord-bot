9e4b095e19666dfadf5b4f347c52f9daad7b7429
Roz <roz@rjmunhoz.me>
2025-10-08 12:01:00 +0200

    refactor(bot): remove old plugin directories
    Clean up old plugin structure after migration to src/bot directory.

--- src/plugins/config/plugin.ts
+++
@@ -1,8 +0,0 @@
-import { guildPlugin } from "knub";
-import { configCommand } from "./commands/config.command";
-
-export const configPlugin = guildPlugin({
-  name: "config",
-  configParser: () => ({}),
-  slashCommands: [configCommand],
-});
--- src/plugins/moderation/actions/delete-message.ts
+++
@@ -1,9 +0,0 @@
-import { defineAction } from "../action";
-
-export const deleteMessageAction = defineAction({
-  name: "delete-message",
-  description: "Delete the message",
-  execute: async ({ message }) => {
-    message.deletable && (await message.delete());
-  },
-});
